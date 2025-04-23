7cc3a18c77199b617d42bb37d7010f33da02ec6d
marco <marcoramosj@MacBook-Air-de-Marco-2.local>
2025-04-23 13:01:11 -0600

    modifique los archivos del api

---
+++ Assets/SaveSystem/BODYREQUESTS.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoadPlayerScore : MonoBehaviour
+{
+    private string APIurl = "http://localhost:5000/";
+    private int OwnerID;
+
+    void Start()
+    {
+        // Leer el ID del jugador desde la URL (si aplica)
+        int questionIndex = Application.absoluteURL.IndexOf("?");
+        if (questionIndex != -1)
+        {
+            string param = Application.absoluteURL.Split('?')[1];
+            int equalIndex = param.IndexOf("=");
+            if (equalIndex != -1)
+            {
+                OwnerID = int.Parse(param.Split('=')[1]);
+            }
+        }
+        else
+        {
+            OwnerID = 1; // valor por defecto
+        }
+
+        StartCoroutine(Load());
+    }
+
+    IEnumerator Load()
+    {
+        string fullURL = APIurl + OwnerID;
+        Debug.Log("Consultando: " + fullURL);
+
+        using (UnityWebRequest web = UnityWebRequest.Get(fullURL))
+        {
+            yield return web.SendWebRequest();
+
+            if (web.result == UnityWebRequest.Result.ConnectionError || web.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error al conectar con la API: " + web.error);
+            }
+            else
+            {
+                Debug.Log("Respuesta: " + web.downloadHandler.text);
+
+                // Si tienes una clase User con highScore
+                PlayerData playerData = JsonUtility.FromJson<PlayerData>(web.downloadHandler.text);
+                Debug.Log("High Score del jugador: " + playerData.highScore);
+
+                // Aquí podrías guardar el high score localmente si quieres
+                SaveManager.SaveHighScore(playerData.highScore);
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class PlayerData
+    {
+        public int OwnerID;
+        public int highScore;
+    }
+}
--- Assets/SaveSystem/post.cs
+++ Assets/SaveSystem/post.cs
@@ -1,8 +1,48 @@
-﻿using System;
-public class NewClass
-{
-	public NewClass()
-	{
-	}
-}
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
 
+public class SendHighScore : MonoBehaviour
+{
+    private string apiUrl = "http://localhost:5000/users/";
+
+    public void EnviarHighScore(int playerID, int highScore)
+    {
+        StartCoroutine(PostHighScore(playerID, highScore));
+    }
+
+    IEnumerator PostHighScore(int playerID, int highScore)
+    {
+        string fullURL = apiUrl + playerID + "/highscore";
+
+        // Creamos el objeto con el score
+        HighScoreData data = new HighScoreData { highScore = highScore };
+        string jsonData = JsonUtility.ToJson(data);
+
+        // Preparamos el request POST
+        UnityWebRequest web = new UnityWebRequest(fullURL, "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(jsonData);
+        web.uploadHandler = new UploadHandlerRaw(jsonToSend);
+        web.downloadHandler = new DownloadHandlerBuffer();
+        web.SetRequestHeader("Content-Type", "application/json");
+
+        // Enviamos y esperamos la respuesta
+        yield return web.SendWebRequest();
+
+        if (web.result == UnityWebRequest.Result.ConnectionError || web.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError("Error al enviar high score: " + web.error);
+        }
+        else
+        {
+            Debug.Log("High score enviado correctamente");
+            Debug.Log("Respuesta: " + web.downloadHandler.text);
+        }
+    }
+
+    [System.Serializable]
+    public class HighScoreData
+    {
+        public int highScore;
+    }
+}
